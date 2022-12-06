4ff4cd8da1ae414b38d4ff86250a5e5f8d31154d
Sostene <jetw.ncc@gmail.com>
2022-12-07 00:51:44 +0100

    ci ho provato, sono le 2 del mattino ner chilling bro

--- stonks.c
+++ stonks.c
@@ -9,32 +9,68 @@
 #define MAXK 3000
 
 // input data
-int N, K, i, j;
-int B[MAXN][MAXK], S[MAXN][MAXK];
+int N, K, i, j;                   // N->number of days, K->number of stocks
+int B[MAXN][MAXK], S[MAXN][MAXK]; // B->buy, S->sell
 
-int createMatrixFromFile(char *filename, int matrix[MAXN][MAXK]) {
-    FILE *fp;
-    int i, j;
-    int N, K;
 
-    fp = fopen(filename, "r");
-    assert(fp != NULL);
-
-    fscanf(fp, "%d %d", &N, &K);
-    for (i=0; i<N; i++)
-        for (j=0; j<K; j++)
-            fscanf(fp, "%d", &matrix[i][j]);
-
-    fclose(fp);
-
-    return N;
+int nVertici(int N, int K)
+{
+    return N * 2 * K;
 }
+
+
+void printMatrix(int N, int K, int B[MAXN][MAXK], int S[MAXN][MAXK])
+{
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < K; j++)
+        {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < K; j++)
+        {
+            printf("%d ", S[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+struct nodo{
+    double peso;
+    struct nodo *next;
+};typedef struct nodo Nodo;
+
+
+Nodo *createPath(int N, int K, int B[MAXN][MAXK], int S[MAXN][MAXK], Nodo *prev)
+{
+
+    //TOFIX: non so come creare il path, non so creare il grafo 
+
+    //creao un nodo che ha il peso dell'elemento dell'array e il puntatore al nodo successivo che è il nodo che ho creato prima
+    //problema è che il nodo precedente non esiste ancora se inizio a creare il path
+    //il path è un array di nodi, quindi devo creare un array di nodi
+    //probabilmente non ha senso salvare il puntatore al nodo successivo, ma solo il peso
+    
+
+    Nodo *nodo = malloc(sizeof(Nodo));
+    nodo->peso = prev->peso / prev->next->peso;
+    nodo->next = malloc(sizeof(Nodo));
+    nodo->next->peso = B[1][1];
+    
+    return nodo;
+}
+
 
 int main()
 {
     //  uncomment the following lines if you want to read/write from files
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
 
     assert(2 == scanf("%d%d", &N, &K));
     for (i = 0; i < N; i++)
@@ -46,7 +82,11 @@ int main()
     }
 
     // insert your code here
+    printMatrix(N, K, B, S);
 
-    printf("%d\n", 42); // print the result
+
+
+    printf("%d\n",42); // print your result
+
     return 0;
 }
